29f7bafdeacc484024ab8d35410b0d3effa8d0c1
Simon <vonsimon2@gmail.com>
2022-07-31 18:00:25 +0200

    final

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-function App() {
+import {Button, Box, Typography} from '@mui/material';
+import { styled } from '@mui/material/styles';
+
+export default function App() {
+  const MyCustomButton = styled(Button)({
+    padding: 10
+  });
+
   return (
-    <div className="App">
-      Hello
+    <div>
+      <Box textAlign='center' sx={{ p:2, border: '1px dashed black'}}>
+        <Typography variant="myVariant" > 
+        Material UI example 
+        </Typography>
+        <Typography sx={{ color: "MyCustomColor.main", m:2 }}> 
+        Example Line 
+        </Typography>
+        <Box textAlign='center'>
+  <MyCustomButton sx={{ m:2 }} color='secondary' variant='contained'>
+     Click here
+  </MyCustomButton>
+</Box>
+</Box>
     </div>
   );
 }
 
-export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { ThemeProvider, createTheme } from '@mui/material';
+import { orange, common, red } from '@mui/material/colors';
 import App from './App';
 
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#CCC"
+    },
+    secondary: {
+      main: orange[500]
+    },
+    myCustomColor: {
+      main: red[400],
+      superDark: red[800],
+      superLight: red[100]
+    }
+  },
+  typography: {
+    myVariant: {
+      fontSize: "2rem",
+      color: common
+    }
+  }
+});
+
+
 root.render(
   <React.StrictMode>
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
